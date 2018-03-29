3c033a43a1112e279ba43f0c8b566d327f6f9397
Aaron Robson <ARobsonSE@hotmail.co.uk>
2018-03-29 19:35:09 +0100

    Simple tests.

---
+++ test_sierpinski_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import unittest
+
+import sierpinski_triangle as st
+
+
+class TestSierpinskiTriangle(unittest.TestCase):
+
+    def test_step_count(self):
+        obj = st.SierpinskiTriangle()
+        self.assertEqual(obj.steps, 0)
+        obj.Step()
+        self.assertEqual(obj.steps, 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ test_sierpinski_triangle_gui.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import unittest
+
+import sierpinski_triangle_gui as gui
+
+
+class TestSierpinskiTriangleGUI(unittest.TestCase):
+
+    def test_gui(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
